f8c51bb8e4d2ad0a680543c4d7cea5921a464253
Kowixo <69046628+Kowixo@users.noreply.github.com>
2021-07-08 19:40:14 +0200

    main commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Juego Plataformas Unity 2D
  Proyecto de Unity Plataformas en 2D
+
+Como Instalarlo:
+
+1- Abre Unity Hub
+2- En la pestaña de Projects, haz click en el botón de "Add"
+3- Selecciona la carpeta "juegoPlataformas" dentro de la carpeta descargada.
+4- Abre el Proyecto: "juegoPlataformas"
---
+++ juegoPlataformas/Assets/playerScript.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer spr;
+
+    public Sprite jumpIMG;
+    public Sprite fallIMG;
+
+    bool canIdle = false;
+    bool canRun = true;
+
+
+    bool playerCanJump = true;
+
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        rb.AddForce(new Vector2(moveX, 0) * speed * Time.deltaTime);
+
+        if(moveY > 0 && playerCanJump)
+        {
+            playerCanJump = false;
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+
+        if(rb.velocity.y > 0)
+        {
+            if(moveX > 0)
+            {
+                spr.flipX = false;
+            }
+            else if(moveX < 0)
+            {
+                spr.flipX = true;
+            }
+            spr.sprite = jumpIMG;
+            anim.enabled = false;
+        }
+        else if(rb.velocity.y < 0)
+        {
+            if (moveX > 0)
+            {
+                spr.flipX = false;
+            }
+            else if (moveX < 0)
+            {
+                spr.flipX = true;
+            }
+            spr.sprite = fallIMG;
+        }
+        else
+        {
+            anim.enabled = true;
+
+            if(moveX > 0 && canRun)
+            {
+                canRun = false;
+                canIdle = true;
+
+                anim.SetTrigger("run");
+                spr.flipX = false;
+            }
+            else if(moveX < 0 && canRun)
+            {
+                canRun = false;
+                canIdle = true;
+
+                anim.SetTrigger("run");
+                spr.flipX = true;
+            }
+            else if(moveX == 0 && canIdle)
+            {
+                canIdle = false;
+                canRun = true;
+
+                anim.SetTrigger("idle");
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("ground"))
+        {
+            playerCanJump = true;
+        }
+    }
+}
